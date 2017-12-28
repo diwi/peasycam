586b12e0d97ec84122a60b62af15c5351fb4abdb
Thomas Diewald <diewald31415@gmail.com>
2017-12-28 19:03:58 +0100

    clean branch

--- resources/code/deploy_web.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-cd /c/europa-winter07/workspace/PeasyCam/distribution/web
-chmod -R a+rwX *
-rsync -azv --exclude '.svn/' --delete -e ssh * mrfeinberg.com:/var/www/html/mrfeinberg.com/peasycam
